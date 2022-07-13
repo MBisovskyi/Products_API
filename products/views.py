bc1f6d0ec7c44d949281776e7c2c2e6e2a13e246
unknown <mbisovskyi@gmail.com>
2022-07-13 15:30:28 -0400

    Updated models.py and views.py for Products App + Created Reviews App

--- products/views.py
+++ products/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
 from rest_framework import status
-from products import serializers
 
 # Create your views here.
 @api_view(['GET', 'POST'])
