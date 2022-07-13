ce4c2c660582e8ec3501f8fe88aa4692a41c2c87
unknown <mbisovskyi@gmail.com>
2022-07-13 14:22:13 -0400

    Updated Product model\, it's requaring an image url

--- products/models.py
+++ products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.CharField(max_length=1000)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    title = models.CharField(max_length = 255)
+    image = models.CharField(max_length = 10000)
+    description = models.CharField(max_length = 1000)
+    price = models.DecimalField(max_digits = 4, decimal_places = 2)
     inventory_quantity = models.IntegerField()
